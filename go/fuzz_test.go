31d3b0f4a120661e5f2072c04b8d3a4ffd1dce70
lfousseaq-robot <107952334+lfousseaq-robot@users.noreply.github.com>
2023-01-18 14:25:42 -0500

    Project import generated by Copybara. (#33)
    FolderOrigin-RevId: /root/project/sandwich
    
    Co-authored-by: Earl of Sandwich <noreply@sandboxaq.com>
    GitOrigin-RevId: 616dd8b948922eb17511dea8a038d7a1be81c15e

--- go/fuzz_test.go
+++ go/fuzz_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package fuzz_test
+package sandwich_test
 
 import (
 	"encoding/base64"
