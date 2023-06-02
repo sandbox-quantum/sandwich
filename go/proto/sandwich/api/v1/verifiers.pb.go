fbe20707634daefc02c4e1a3f929fb0eae36c895
lfousseaq-robot <107952334+lfousseaq-robot@users.noreply.github.com>
2023-06-02 10:38:20 +0200

    Project import generated by Copybara. (#116)
    FolderOrigin-RevId: /home/pawn/project/sandwich
    
    Co-authored-by: Earl of Sandwich <noreply@sandboxaq.com>
    GitOrigin-RevId: 8f2119f48c2da06bb053f22c6c003ba5b17932c5

--- go/proto/sandwich/api/v1/verifiers.pb.go
+++ go/proto/sandwich/api/v1/verifiers.pb.go
@@ -151,6 +151,227 @@ func (x *X509Verifier) GetMaxVerifyDepth() uint32 {
 	return 0
 }
 
+type SANMatcher struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// Types that are assignable to San:
+	//	*SANMatcher_Email
+	//	*SANMatcher_Dns
+	//	*SANMatcher_IpAddress
+	San isSANMatcher_San `protobuf_oneof:"san"`
+}
+
+func (x *SANMatcher) Reset() {
+	*x = SANMatcher{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_proto_api_v1_verifiers_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *SANMatcher) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*SANMatcher) ProtoMessage() {}
+
+func (x *SANMatcher) ProtoReflect() protoreflect.Message {
+	mi := &file_proto_api_v1_verifiers_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use SANMatcher.ProtoReflect.Descriptor instead.
+func (*SANMatcher) Descriptor() ([]byte, []int) {
+	return file_proto_api_v1_verifiers_proto_rawDescGZIP(), []int{2}
+}
+
+func (m *SANMatcher) GetSan() isSANMatcher_San {
+	if m != nil {
+		return m.San
+	}
+	return nil
+}
+
+func (x *SANMatcher) GetEmail() string {
+	if x, ok := x.GetSan().(*SANMatcher_Email); ok {
+		return x.Email
+	}
+	return ""
+}
+
+func (x *SANMatcher) GetDns() string {
+	if x, ok := x.GetSan().(*SANMatcher_Dns); ok {
+		return x.Dns
+	}
+	return ""
+}
+
+func (x *SANMatcher) GetIpAddress() string {
+	if x, ok := x.GetSan().(*SANMatcher_IpAddress); ok {
+		return x.IpAddress
+	}
+	return ""
+}
+
+type isSANMatcher_San interface {
+	isSANMatcher_San()
+}
+
+type SANMatcher_Email struct {
+	Email string `protobuf:"bytes,1,opt,name=email,proto3,oneof"`
+}
+
+type SANMatcher_Dns struct {
+	Dns string `protobuf:"bytes,2,opt,name=dns,proto3,oneof"`
+}
+
+type SANMatcher_IpAddress struct {
+	IpAddress string `protobuf:"bytes,3,opt,name=ip_address,json=ipAddress,proto3,oneof"`
+}
+
+func (*SANMatcher_Email) isSANMatcher_San() {}
+
+func (*SANMatcher_Dns) isSANMatcher_San() {}
+
+func (*SANMatcher_IpAddress) isSANMatcher_San() {}
+
+type SANVerifier struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	AltNames []*SANMatcher `protobuf:"bytes,1,rep,name=alt_names,json=altNames,proto3" json:"alt_names,omitempty"`
+}
+
+func (x *SANVerifier) Reset() {
+	*x = SANVerifier{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_proto_api_v1_verifiers_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *SANVerifier) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*SANVerifier) ProtoMessage() {}
+
+func (x *SANVerifier) ProtoReflect() protoreflect.Message {
+	mi := &file_proto_api_v1_verifiers_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use SANVerifier.ProtoReflect.Descriptor instead.
+func (*SANVerifier) Descriptor() ([]byte, []int) {
+	return file_proto_api_v1_verifiers_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *SANVerifier) GetAltNames() []*SANMatcher {
+	if x != nil {
+		return x.AltNames
+	}
+	return nil
+}
+
+type TunnelVerifier struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// Types that are assignable to Verifier:
+	//	*TunnelVerifier_SanVerifier
+	//	*TunnelVerifier_EmptyVerifier
+	Verifier isTunnelVerifier_Verifier `protobuf_oneof:"verifier"`
+}
+
+func (x *TunnelVerifier) Reset() {
+	*x = TunnelVerifier{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_proto_api_v1_verifiers_proto_msgTypes[4]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *TunnelVerifier) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*TunnelVerifier) ProtoMessage() {}
+
+func (x *TunnelVerifier) ProtoReflect() protoreflect.Message {
+	mi := &file_proto_api_v1_verifiers_proto_msgTypes[4]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use TunnelVerifier.ProtoReflect.Descriptor instead.
+func (*TunnelVerifier) Descriptor() ([]byte, []int) {
+	return file_proto_api_v1_verifiers_proto_rawDescGZIP(), []int{4}
+}
+
+func (m *TunnelVerifier) GetVerifier() isTunnelVerifier_Verifier {
+	if m != nil {
+		return m.Verifier
+	}
+	return nil
+}
+
+func (x *TunnelVerifier) GetSanVerifier() *SANVerifier {
+	if x, ok := x.GetVerifier().(*TunnelVerifier_SanVerifier); ok {
+		return x.SanVerifier
+	}
+	return nil
+}
+
+func (x *TunnelVerifier) GetEmptyVerifier() *EmptyVerifier {
+	if x, ok := x.GetVerifier().(*TunnelVerifier_EmptyVerifier); ok {
+		return x.EmptyVerifier
+	}
+	return nil
+}
+
+type isTunnelVerifier_Verifier interface {
+	isTunnelVerifier_Verifier()
+}
+
+type TunnelVerifier_SanVerifier struct {
+	SanVerifier *SANVerifier `protobuf:"bytes,1,opt,name=san_verifier,json=sanVerifier,proto3,oneof"`
+}
+
+type TunnelVerifier_EmptyVerifier struct {
+	EmptyVerifier *EmptyVerifier `protobuf:"bytes,2,opt,name=empty_verifier,json=emptyVerifier,proto3,oneof"`
+}
+
+func (*TunnelVerifier_SanVerifier) isTunnelVerifier_Verifier() {}
+
+func (*TunnelVerifier_EmptyVerifier) isTunnelVerifier_Verifier() {}
+
 var File_proto_api_v1_verifiers_proto protoreflect.FileDescriptor
 
 var file_proto_api_v1_verifiers_proto_rawDesc = []byte{
@@ -179,12 +400,35 @@ var file_proto_api_v1_verifiers_proto_rawDesc = []byte{
 	0x70, 0x69, 0x72, 0x65, 0x64, 0x43, 0x65, 0x72, 0x74, 0x69, 0x66, 0x69, 0x63, 0x61, 0x74, 0x65,
 	0x12, 0x28, 0x0a, 0x10, 0x6d, 0x61, 0x78, 0x5f, 0x76, 0x65, 0x72, 0x69, 0x66, 0x79, 0x5f, 0x64,
 	0x65, 0x70, 0x74, 0x68, 0x18, 0x05, 0x20, 0x01, 0x28, 0x0d, 0x52, 0x0e, 0x6d, 0x61, 0x78, 0x56,
-	0x65, 0x72, 0x69, 0x66, 0x79, 0x44, 0x65, 0x70, 0x74, 0x68, 0x42, 0x3e, 0x5a, 0x3c, 0x67, 0x69,
-	0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x61, 0x6e, 0x64, 0x62, 0x6f, 0x78,
-	0x2d, 0x71, 0x75, 0x61, 0x6e, 0x74, 0x75, 0x6d, 0x2f, 0x73, 0x61, 0x6e, 0x64, 0x77, 0x69, 0x63,
-	0x68, 0x2f, 0x67, 0x6f, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x73, 0x61, 0x6e, 0x64, 0x77,
-	0x69, 0x63, 0x68, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x76, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
-	0x6f, 0x33,
+	0x65, 0x72, 0x69, 0x66, 0x79, 0x44, 0x65, 0x70, 0x74, 0x68, 0x22, 0x60, 0x0a, 0x0a, 0x53, 0x41,
+	0x4e, 0x4d, 0x61, 0x74, 0x63, 0x68, 0x65, 0x72, 0x12, 0x16, 0x0a, 0x05, 0x65, 0x6d, 0x61, 0x69,
+	0x6c, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x48, 0x00, 0x52, 0x05, 0x65, 0x6d, 0x61, 0x69, 0x6c,
+	0x12, 0x12, 0x0a, 0x03, 0x64, 0x6e, 0x73, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x48, 0x00, 0x52,
+	0x03, 0x64, 0x6e, 0x73, 0x12, 0x1f, 0x0a, 0x0a, 0x69, 0x70, 0x5f, 0x61, 0x64, 0x64, 0x72, 0x65,
+	0x73, 0x73, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x48, 0x00, 0x52, 0x09, 0x69, 0x70, 0x41, 0x64,
+	0x64, 0x72, 0x65, 0x73, 0x73, 0x42, 0x05, 0x0a, 0x03, 0x73, 0x61, 0x6e, 0x22, 0x51, 0x0a, 0x0b,
+	0x53, 0x41, 0x4e, 0x56, 0x65, 0x72, 0x69, 0x66, 0x69, 0x65, 0x72, 0x12, 0x42, 0x0a, 0x09, 0x61,
+	0x6c, 0x74, 0x5f, 0x6e, 0x61, 0x6d, 0x65, 0x73, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x25,
+	0x2e, 0x73, 0x61, 0x71, 0x2e, 0x73, 0x61, 0x6e, 0x64, 0x77, 0x69, 0x63, 0x68, 0x2e, 0x70, 0x72,
+	0x6f, 0x74, 0x6f, 0x2e, 0x61, 0x70, 0x69, 0x2e, 0x76, 0x31, 0x2e, 0x53, 0x41, 0x4e, 0x4d, 0x61,
+	0x74, 0x63, 0x68, 0x65, 0x72, 0x52, 0x08, 0x61, 0x6c, 0x74, 0x4e, 0x61, 0x6d, 0x65, 0x73, 0x22,
+	0xbc, 0x01, 0x0a, 0x0e, 0x54, 0x75, 0x6e, 0x6e, 0x65, 0x6c, 0x56, 0x65, 0x72, 0x69, 0x66, 0x69,
+	0x65, 0x72, 0x12, 0x4b, 0x0a, 0x0c, 0x73, 0x61, 0x6e, 0x5f, 0x76, 0x65, 0x72, 0x69, 0x66, 0x69,
+	0x65, 0x72, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x26, 0x2e, 0x73, 0x61, 0x71, 0x2e, 0x73,
+	0x61, 0x6e, 0x64, 0x77, 0x69, 0x63, 0x68, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2e, 0x61, 0x70,
+	0x69, 0x2e, 0x76, 0x31, 0x2e, 0x53, 0x41, 0x4e, 0x56, 0x65, 0x72, 0x69, 0x66, 0x69, 0x65, 0x72,
+	0x48, 0x00, 0x52, 0x0b, 0x73, 0x61, 0x6e, 0x56, 0x65, 0x72, 0x69, 0x66, 0x69, 0x65, 0x72, 0x12,
+	0x51, 0x0a, 0x0e, 0x65, 0x6d, 0x70, 0x74, 0x79, 0x5f, 0x76, 0x65, 0x72, 0x69, 0x66, 0x69, 0x65,
+	0x72, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x28, 0x2e, 0x73, 0x61, 0x71, 0x2e, 0x73, 0x61,
+	0x6e, 0x64, 0x77, 0x69, 0x63, 0x68, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2e, 0x61, 0x70, 0x69,
+	0x2e, 0x76, 0x31, 0x2e, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x56, 0x65, 0x72, 0x69, 0x66, 0x69, 0x65,
+	0x72, 0x48, 0x00, 0x52, 0x0d, 0x65, 0x6d, 0x70, 0x74, 0x79, 0x56, 0x65, 0x72, 0x69, 0x66, 0x69,
+	0x65, 0x72, 0x42, 0x0a, 0x0a, 0x08, 0x76, 0x65, 0x72, 0x69, 0x66, 0x69, 0x65, 0x72, 0x42, 0x3e,
+	0x5a, 0x3c, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x61, 0x6e,
+	0x64, 0x62, 0x6f, 0x78, 0x2d, 0x71, 0x75, 0x61, 0x6e, 0x74, 0x75, 0x6d, 0x2f, 0x73, 0x61, 0x6e,
+	0x64, 0x77, 0x69, 0x63, 0x68, 0x2f, 0x67, 0x6f, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x73,
+	0x61, 0x6e, 0x64, 0x77, 0x69, 0x63, 0x68, 0x2f, 0x61, 0x70, 0x69, 0x2f, 0x76, 0x31, 0x62, 0x06,
+	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -199,19 +443,25 @@ func file_proto_api_v1_verifiers_proto_rawDescGZIP() []byte {
 	return file_proto_api_v1_verifiers_proto_rawDescData
 }
 
-var file_proto_api_v1_verifiers_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_proto_api_v1_verifiers_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
 var file_proto_api_v1_verifiers_proto_goTypes = []interface{}{
-	(*EmptyVerifier)(nil), // 0: saq.sandwich.proto.api.v1.EmptyVerifier
-	(*X509Verifier)(nil),  // 1: saq.sandwich.proto.api.v1.X509Verifier
-	(*Certificate)(nil),   // 2: saq.sandwich.proto.api.v1.Certificate
+	(*EmptyVerifier)(nil),  // 0: saq.sandwich.proto.api.v1.EmptyVerifier
+	(*X509Verifier)(nil),   // 1: saq.sandwich.proto.api.v1.X509Verifier
+	(*SANMatcher)(nil),     // 2: saq.sandwich.proto.api.v1.SANMatcher
+	(*SANVerifier)(nil),    // 3: saq.sandwich.proto.api.v1.SANVerifier
+	(*TunnelVerifier)(nil), // 4: saq.sandwich.proto.api.v1.TunnelVerifier
+	(*Certificate)(nil),    // 5: saq.sandwich.proto.api.v1.Certificate
 }
 var file_proto_api_v1_verifiers_proto_depIdxs = []int32{
-	2, // 0: saq.sandwich.proto.api.v1.X509Verifier.trusted_cas:type_name -> saq.sandwich.proto.api.v1.Certificate
-	1, // [1:1] is the sub-list for method output_type
-	1, // [1:1] is the sub-list for method input_type
-	1, // [1:1] is the sub-list for extension type_name
-	1, // [1:1] is the sub-list for extension extendee
-	0, // [0:1] is the sub-list for field type_name
+	5, // 0: saq.sandwich.proto.api.v1.X509Verifier.trusted_cas:type_name -> saq.sandwich.proto.api.v1.Certificate
+	2, // 1: saq.sandwich.proto.api.v1.SANVerifier.alt_names:type_name -> saq.sandwich.proto.api.v1.SANMatcher
+	3, // 2: saq.sandwich.proto.api.v1.TunnelVerifier.san_verifier:type_name -> saq.sandwich.proto.api.v1.SANVerifier
+	0, // 3: saq.sandwich.proto.api.v1.TunnelVerifier.empty_verifier:type_name -> saq.sandwich.proto.api.v1.EmptyVerifier
+	4, // [4:4] is the sub-list for method output_type
+	4, // [4:4] is the sub-list for method input_type
+	4, // [4:4] is the sub-list for extension type_name
+	4, // [4:4] is the sub-list for extension extendee
+	0, // [0:4] is the sub-list for field type_name
 }
 
 func init() { file_proto_api_v1_verifiers_proto_init() }
@@ -245,6 +495,51 @@ func file_proto_api_v1_verifiers_proto_init() {
 				return nil
 			}
 		}
+		file_proto_api_v1_verifiers_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*SANMatcher); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_proto_api_v1_verifiers_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*SANVerifier); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_proto_api_v1_verifiers_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*TunnelVerifier); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+	}
+	file_proto_api_v1_verifiers_proto_msgTypes[2].OneofWrappers = []interface{}{
+		(*SANMatcher_Email)(nil),
+		(*SANMatcher_Dns)(nil),
+		(*SANMatcher_IpAddress)(nil),
+	}
+	file_proto_api_v1_verifiers_proto_msgTypes[4].OneofWrappers = []interface{}{
+		(*TunnelVerifier_SanVerifier)(nil),
+		(*TunnelVerifier_EmptyVerifier)(nil),
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -252,7 +547,7 @@ func file_proto_api_v1_verifiers_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_proto_api_v1_verifiers_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   5,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
